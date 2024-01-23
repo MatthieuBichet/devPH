3a10d5e0e1260ee447e2371287f0c67f3611a4ac
MattB <matthieu.bichet@gmail.com>
2024-01-23 17:49:23 +0100

    OK, prêt pour test live

--- app.js
+++ app.js
@@ -11,59 +11,48 @@ app.locals.basedir = path.join(__dirname, 'views');
 const { platform } = process;
 const locale = path[platform === `win32` ? `win32` : `posix`];
 
-
+//cheminFichier = variable de base des documents
 var cheminFichier = config.get("cheminFichier");
 var cheminFichierNor = cheminFichier.split(path.sep).join(locale.sep);
 
 
-var docsPR = [];           
-var foldersPR = [];  
-var folderLib =[];
-var docLib =[];
+var docsPR = [];    //dirent array des fichiers du système de docs      
+var foldersPR = []; //dirent array des dossiers du système de docs
+var folderLib =[];  //string array des noms de dossier du système de docs
+var docLib =[];     //string array des noms de fichiers du sytème de docs
 initDB();
 
 app.get("", (req, res) => {
-  
+//gestion des requêtes de recherche  
+
+//récupération des paramètres de recherche
 var queryName = url.parse(req.url, true).query.name;
 var queryFolder = url.parse(req.url, true).query.folder;
-var readDoc= [];
-var readFolder = folderLib;
-var readDocsPR =[];
+//variables de travail
+var readFolder = folderLib; //array des dossiers manipulable
+var readDocsPR =[];         //array des fichiers manipulable
 
-
+//filtrage via paramètre de dossier / nom de fichier
 try{
-
+    //recherche globale ou par fichier(sous fichier)
     if(queryFolder != cheminFichierNor && typeof queryFolder ==='string')
     {
-        /*for(const doc of docsPR)
-        {
-          if(doc.path == queryFolder)
-          {
-            readDoc.push(doc.name);
-          }
-
-        
-        }*/
         readDocsPR = docsPR;
         readDocsPR = readDocsPR.filter(el => el.path == queryFolder);
         readDocsPR.forEach(el => el.name.toLowerCase());
-        //readDoc = docsPR.filter(el => {return el.path == queryFolder})
-        //readDoc = readDoc.map(word =>{return word.name.toLowerCase();});
-        
-      
     }
     else
     {
       readDocsPR = docsPR;
 
     }
+    //filtrage par nom de fichier
     if(typeof queryName ==='string')
     {
       readDocsPR = readDocsPR.filter(word=> word.name.includes(queryName.toLowerCase()));
 
     }
-    //if(typeof )
-    //verifier que la query soit non nulle
+    //envoi des résultats de recherche
     res.render("template", {docs: readDocsPR, folders: readFolder, nameSearch: queryName, folderSearch: queryFolder});
     res.statusCode = 200;
 
@@ -77,7 +66,7 @@ catch(error)
 })
 
 
-
+//requête d'ouverture de fichier
 app.get("/opendoc", (req, res) =>{
   res.statusCode = 200;
   var q = url.parse(req.url, true).query;
@@ -94,21 +83,29 @@ app.get("/opendoc", (req, res) =>{
 
   
 })*/
+
+//initialisation de la librairie de fichiers / noms de dossiers
 function initDB()
 {
   folderLib.length=0;
   docLib.length=0;
-  var docs = getFiles(cheminFichier);            //récup des fichiers async
+  //requêtes asynchrones via fs.readdir
+  var docs = getFiles(cheminFichier);           
   var folders = getFolders(cheminFichier);
+  
+  //attente de la réponse
   docs.then(function(docsResult)
   {
     folders.then(function(folderResult)
     {
+      //copie des libraires en variables accessibles
       docsPR = docsResult;
       foldersPR = folderResult;
+      //ajout du root à la liste
       folderLib.push(cheminFichierNor);
       for(const folder of folderResult)
       {
+        //gestion des path win / posix
         folder.path = folder.path.split(path.sep).join(locale.sep);
         if(folder.path.charAt(folder.path.length-1)!=locale.sep)
         {
@@ -116,7 +113,7 @@ function initDB()
         }
         folderLib.push(path.join(folder.path + (folder.name)));
       }
-    
+    //complétion des libraires de noms
     for(const doc of docsResult)
     {
       docLib.push(doc.name);
@@ -128,34 +125,41 @@ function initDB()
     docLib = docLib.map(word =>{return word.toLowerCase();});
   });
 });
-
 }
+
+//requête d'actualisation de la liste des fichiers
 app.get("/refresh", (req, res)=>{
   initDB();
   res.redirect("/");
 })
 
+//gestion d'erreur
 app.use((req, res) => {
     res.statusCode = 404;
     res.end("404 - page not found");
 });
 
+//ouverture du serveur au port 3000 par défaut TODO: ajouter si nécessaire la gestion du port dans le fichier de config
 app.listen(3000, () => {
 
     console.log("Application started on port 3000");
 });
 
+//fonction asynchrone de recherche des fichiers de manière récursive
 async function getFiles(dir) {
   return await fsp.readdir(dir, { recursive: true, withFileTypes: true }).then
   ((recherche) =>{
+  //filtrage par type : fichier
   recherche = recherche.filter(dirent => dirent.isFile());
   return recherche;
 })
 }
 
+//fonction asynchrone de recherche des dossier de manière récursive
 async function getFolders(dir){
   return await fsp.readdir(dir, { recursive: true, withFileTypes: true }).then
   ((recherche) =>{
+    //filtrage par type : dossier
     recherche = recherche.filter(dirent => dirent.isDirectory());
     return recherche;
     
